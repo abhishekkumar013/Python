5647613332f200875d288a7d6ae1c73902745753
Abhishek kumar <sahaabhishek443@gmail.com>
2025-01-25 19:12:00 +0530

    Dictionary & Set  question

--- Dictionary_Set/question.py
+++ Dictionary_Set/question.py
@@ -23,3 +23,25 @@ print(len(sets))
 
 classes={"python", "java", "C++", "python", "javascript","java", "python", "java", "C++" , "C"}
 print(len(classes))
+
+#WAP to enter marks of 3 subjects from the user and store them in a dictionary. Start with an empty dictionary & add one by one. Use subject name as key & marks as value.
+list1=["phy","chem","math"]
+dict1={}
+
+for i in range(len(list1)):
+    x=int(input(f"Enter marks for {list1[i]}"))
+    dict1[list1[i]]=x
+
+print(dict1)
+
+# Figure out a way to store 9 & 9.0 as separate values in the set.(You can take help of built-in data types)
+
+value={9,9.0}
+print(value)
+
+
+values={
+("float", 9.0),
+("int", 9)
+}
+print(values)
